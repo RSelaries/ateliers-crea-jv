9655291772e2ec78df39ce5a2f8b2a7d17e50b38
Selaries Raphael <raphael.selaries@ensaama.net>
2026-07-31 21:04:52 +0200

    Remove redirect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,5 @@
 	<title>Ateliers création JV</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<a href="{base}/ateliers/walking-simulator">Walking sim</a>
+<h1>Test in root</h1>
+<a href="{base}/introduction">Ateliers de création</a>
